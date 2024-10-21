8cec2d76453caaca33888ef87f1d12e5062669c4
italoalcantaraa <italoalcantara6@gmail.com>
2024-10-20 22:30:42 -0300

    componente do cabecalho simples

--- public/src/components/header/simpleHeader.js
+++ public/src/components/header/simpleHeader.js
@@ -1,7 +1,7 @@
 const head = document.head;
 const body = document.body;
 
-const style = "<link rel='stylesheet' href='simpleHeader.css'>"
+const style = "<link rel='stylesheet' href='../../components/header/StyleHeader.css'>"
 head.innerHTML += style
 
 const header = document.createElement("div")
@@ -9,12 +9,17 @@ header.setAttribute("id", "simple-header")
 header.setAttribute("class", "simple-header")
 body.append(header)
 
-
-const headerContent = 
+const headerContent =
     "<header>" +
+        "<img src='../../assets/images/logo-simples.png'/>" +
         "<div>" +
-            "<img src=''>"
-        "<div>" +
-    "<header/>"
+            "<img src='../../assets/images/user-svgrepo-com.svg'/>" +
+            "<p>" +
+                "<strong>Entre</strong>" + " ou " + "<strong>Cadastre-se</strong>" +
+            "</p>" +
+        "</div>" +
+    "</header>"
+
+header.innerHTML += headerContent
 
 
