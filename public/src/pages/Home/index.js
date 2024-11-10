60ca7899d1604ff4ecdf53e49f2ee1c6cd263953
Alnfire <alisson.s28356@gmail.com>
2024-11-10 19:33:21 -0300

    Primeira tentativa de fazer um popup decente, EU TENTEI!

--- public/src/pages/Home/index.js
+++ public/src/pages/Home/index.js
@@ -15,3 +15,31 @@ function toggleDropdown() {
   var dropdown = document.getElementById("dropdown");
   dropdown.classList.toggle("show");
 }
+
+// function de add no popup
+
+// function remover produto
+
+const removeProduts = document.getElementsByClassName("removerPro")
+for (var i = 0; i < removeProduts.length; i++) {
+  removeProduts[i].addEventListener("click", function (event) {
+    event.target.parentElement.parentElement.remove()
+    updateTotal()
+  })
+}
+
+
+function updateTotal() {
+
+  let totalAmount = 0
+  const cartProducts = document.getElementsByClassName("cardProduto")
+  for (var i = 0; i < cartProducts.length; i++) {
+    const precoProduto = cartProducts[i].getElementsByClassName("valorProduto")[0].innerText.replace("R$", "").replace(",", ".")
+    const quantidadeProduto = cartProducts[i].getElementsByClassName("qtdProduto")[0].value
+
+    totalAmount += precoProduto * quantidadeProduto
+  }
+  totalAmount = totalAmount.toFixed(2)
+  totalAmount = totalAmount.replace(".", ",")
+  document.querySelector(".totalProduto span").innerText = "R$" + totalAmount;
+}
