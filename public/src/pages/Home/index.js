bbfdc703318dfd872e18519521c5621ea4c41a89
Alnfire <alisson.s28356@gmail.com>
2024-11-11 22:48:13 -0300

    HOME ATUALIZADA PELO GRANDE ITALO

--- public/src/pages/Home/index.js
+++ public/src/pages/Home/index.js
@@ -1,4 +1,14 @@
 
+class Produto{
+    constructor(img, nome, preco) {
+      this.img = img;
+      this.nome = nome;
+      this.preco = preco;
+    }
+}
+
+var produtosCarrinho = [];
+
 
 function abrirPopup() {
   document.getElementById("meuPopup").style.display = "flex";
@@ -16,30 +26,65 @@ function toggleDropdown() {
   dropdown.classList.toggle("show");
 }
 
+
+function addCarrinho() {
+  let title = document.getElementById('title').innerHTML;
+  let price = document.getElementById('preco').innerHTML;
+  let img = document.querySelector('#img');
+  let urlImg = img.src;
+  
+  let produto = new Produto(urlImg, title, price);
+
+  produtosCarrinho.push(produto);
+
+  produtosCarrinho.forEach(element => {
+    console.log(element);
+  });
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // function de add no popup
 
 // function remover produto
 
-const removeProduts = document.getElementsByClassName("removerPro")
-for (var i = 0; i < removeProduts.length; i++) {
-  removeProduts[i].addEventListener("click", function (event) {
-    event.target.parentElement.parentElement.remove()
-    updateTotal()
-  })
-}
+// const removeProduts = document.getElementsByClassName("removerPro")
+// for (var i = 0; i < removeProduts.length; i++) {
+//   removeProduts[i].addEventListener("click", function (event) {
+//     event.target.parentElement.parentElement.remove()
+//     updateTotal()
+//   })
+// }
 
 
-function updateTotal() {
+// function updateTotal() {
 
-  let totalAmount = 0
-  const cartProducts = document.getElementsByClassName("cardProduto")
-  for (var i = 0; i < cartProducts.length; i++) {
-    const precoProduto = cartProducts[i].getElementsByClassName("valorProduto")[0].innerText.replace("R$", "").replace(",", ".")
-    const quantidadeProduto = cartProducts[i].getElementsByClassName("qtdProduto")[0].value
+//   let totalAmount = 0
+//   const cartProducts = document.getElementsByClassName("cardProduto")
+//   for (var i = 0; i < cartProducts.length; i++) {
+//     const precoProduto = cartProducts[i].getElementsByClassName("valorProduto")[0].innerText.replace("R$", "").replace(",", ".")
+//     const quantidadeProduto = cartProducts[i].getElementsByClassName("qtdProduto")[0].value
 
-    totalAmount += precoProduto * quantidadeProduto
-  }
-  totalAmount = totalAmount.toFixed(2)
-  totalAmount = totalAmount.replace(".", ",")
-  document.querySelector(".totalProduto span").innerText = "R$" + totalAmount;
-}
+//     totalAmount += precoProduto * quantidadeProduto
+//   }
+//   totalAmount = totalAmount.toFixed(2)
+//   totalAmount = totalAmount.replace(".", ",")
+//   document.querySelector(".totalProduto span").innerText = "R$" + totalAmount;
+// }
