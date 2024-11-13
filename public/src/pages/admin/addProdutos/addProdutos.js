35b85bd21ebf0c2141511a21e6db54126a52c627
Alnfire <alisson.s28356@gmail.com>
2024-11-13 20:28:14 -0300

    Add produtos

---
+++ public/src/pages/admin/addProdutos/addProdutos.js
@@ -0,0 +1,8 @@
+function addProduto() {
+    let categoria = document.getElementById('categoria').innerHTML
+    let name = document.getElementById('name').innerHTML
+    let fabricante = document.getElementById('fabricante').innerHTML
+    let condicao = document.getElementById('condicao').innerHTML
+    let valor = document.getElementById('valor').innerHTML
+    let estoque = document.getElementById('estoque').innerHTML
+}
