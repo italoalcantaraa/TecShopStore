48c7c8d6bfd3267439ef56cc7fc0cf563956ffc3
Luis Moreira <luis010101work@outlook.com>
2024-11-16 13:17:22 -0300

    Animation

---
+++ public/src/script/particles-init.js
@@ -0,0 +1,5 @@
+particlesJS.load('particles-js', '../../script/particlesjs-config.json', function() {
+  console.log('Particles.js carregado com sucesso!');
+});
+
+//TecShopStore/public/src/pages/Home/teste.htm
