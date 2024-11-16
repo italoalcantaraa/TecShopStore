c60b17f4aeb8298c057d3394e45f762ddfb84d58
Luis Moreira <luis010101work@outlook.com>
2024-11-15 23:57:20 -0300

    animate

--- public/src/pages/admin/produtos/produto.js
+++ public/src/pages/admin/produtos/produto.js
@@ -1,3 +1,4 @@
+
 let arrayProducts = [];
 
 window.onload = () => {
@@ -14,10 +15,10 @@ window.onload = () => {
                 <p id="description">${element.description}</p>
                 <p id="price">R$ ${element.price}</p>
                 <div>
-                <button onclick="">
+                <button onclick="deleteProduto('${element.id}')">
                     <img src="../../../assets/images/adm/pencil.svg" alt="">
                 </button>
-                <button onclick="">
+                <button onclick="deleteProduto('${element.id}')">
                     <img src="../../../assets/images/adm/trash.svg" alt="">
                 </button>
                 </div>
@@ -25,8 +26,59 @@ window.onload = () => {
 
             classProducts.innerHTML += product;
     });
+
+
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     CarregarComponente('../../../components/menu/menu.html', 'menu');
 });
+
+function deleteProduto(id){
+
+arrayProducts = JSON.parse(localStorage.getItem('products')) || [];
+
+arrayProducts.forEach(element => {
+    if(element.id == id){
+
+        arrayProducts.splice(arrayProducts.indexOf(element), 1)
+        console.log(element.id)
+    }
+})
+
+localStorage.products = JSON.stringify(arrayProducts)
+
+atualizarListaDeProdutos();
+
+alert(`Produto com ID ${id} removido!`);
+}
+
+function atualizarListaDeProdutos() {
+    let classProducts = document.querySelector('.products');
+    
+    // Limpar o conteúdo da lista de produtos atual
+    classProducts.innerHTML = '';
+
+    arrayProducts = JSON.parse(localStorage.getItem('products')) || [];
+
+    arrayProducts.forEach(element => {
+        const product = `
+            <div class="product">
+                <img id="img-product" src="${element.img}" alt="">
+                <p id="id">${element.id}</p>
+                <p id="name">${element.name}</p>
+                <p id="description">${element.description}</p>
+                <p id="price">R$ ${element.price}</p>
+                <div>
+                    <button onclick="deleteProduto('${element.id}')">
+                        <img src="../../../assets/images/adm/pencil.svg" alt="">
+                    </button>
+                    <button onclick="deleteProduto('${element.id}')">
+                        <img src="../../../assets/images/adm/trash.svg" alt="">
+                    </button>
+                </div>
+            </div>`;
+
+        classProducts.innerHTML += product;
+    });
+}
