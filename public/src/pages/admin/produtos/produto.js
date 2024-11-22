b77e3c7724afbbbac3ad3d468da4de0ee1cce28f
Italo Alcântara <italoalcantara6@gmail.com>
2024-11-22 20:56:06 -0300

    FINALIZADO!!!

--- public/src/pages/admin/produtos/produto.js
+++ public/src/pages/admin/produtos/produto.js
@@ -11,8 +11,8 @@ window.onload = () => {
             <div class="product">
                 <img id="img-product" src="${element.img}" alt="">
                 <p id="id">${element.id}</p>
-                <p id="name">${element.name}</p>
-                <p id="description">${element.description}</p>
+                <p id="name">${element.name.substring(10, 0)}</p>
+                <p id="description">${element.stock}</p>
                 <p id="price">R$ ${element.price}</p>
                 <div>
                 <button onclick="editProduct('${element.id}')">
@@ -24,7 +24,7 @@ window.onload = () => {
                 </div>
             </div> `;
 
-            classProducts.innerHTML += product;
+        classProducts.innerHTML += product;
     });
 
 
@@ -34,40 +34,41 @@ document.addEventListener('DOMContentLoaded', () => {
     CarregarComponente('../../../components/menu/menu.html', 'menu');
 });
 
-function deleteProduto(id){
+function deleteProduto(id) {
 
-arrayProducts = JSON.parse(localStorage.getItem('products')) || [];
+    arrayProducts = JSON.parse(localStorage.getItem('products')) || [];
 
-arrayProducts.forEach(element => {
-    if(element.id == id){
+    arrayProducts.forEach(element => {
+        if (element.id == id) {
 
-        arrayProducts.splice(arrayProducts.indexOf(element), 1)
-        console.log(element.id)
-    }
-})
+            arrayProducts.splice(arrayProducts.indexOf(element), 1)
+            console.log(element.id)
+        }
+    })
 
-localStorage.products = JSON.stringify(arrayProducts)
+    localStorage.products = JSON.stringify(arrayProducts)
 
-atualizarListaDeProdutos();
+    atualizarListaDeProdutos();
 
-alert(`Produto com ID ${id} removido!`);
+    alert(`Produto com ID ${id} removido!`);
 }
 
 function atualizarListaDeProdutos() {
     let classProducts = document.querySelector('.products');
-    
+
     // Limpar o conteúdo da lista de produtos atual
     classProducts.innerHTML = '';
 
     arrayProducts = JSON.parse(localStorage.getItem('products')) || [];
 
     arrayProducts.forEach(element => {
+        ;
         const product = `
             <div class="product">
                 <img id="img-product" src="${element.img}" alt="">
                 <p id="id">${element.id}</p>
-                <p id="name">${element.name}</p>
-                <p id="description">${element.description}</p>
+                <p id="name">${element.name.substring(10, 0)}</p>
+                <p id="description">${element.stock}</p>
                 <p id="price">R$ ${element.price}</p>
                 <div>
                     <button onclick="editProduct(${element.id})">
